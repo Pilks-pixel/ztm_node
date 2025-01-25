40ce816a87282a71f08078059bb94b59bf2b1b6f
Pilks-pixel <peterpilkington@hotmail.com>
2025-01-25 23:07:09 +0000

    Set up Husky pre-commit hook with validate script

--- planets_search/index.ts
+++ planets_search/index.ts
@@ -4,47 +4,47 @@ import { parse } from "csv-parse";
 var csvFilePath = "./kepler-data.csv";
 var habitablePlanets: Planet[] = [];
 var parser = parse({
-	comment: "#",
-	columns: true,
+  comment: "#",
+  columns: true,
 });
 
 type Planet = {
-	kepoi_name: string;
-	koi_disposition: string;
-	koi_insol: number;
-	koi_prad: number;
+  kepoi_name: string;
+  koi_disposition: string;
+  koi_insol: number;
+  koi_prad: number;
 };
 
 parser.on("readable", function () {
-	let record: Planet | null;
-	while ((record = parser.read()) !== null) {
-		if (isHabitablePlanet(record)) {
-			console.log(record);
-			habitablePlanets.push(record);
-		}
-	}
+  let record: Planet | null;
+  while ((record = parser.read()) !== null) {
+    if (isHabitablePlanet(record)) {
+      console.log(record);
+      habitablePlanets.push(record);
+    }
+  }
 });
 
 parser.on("end", function () {
-	console.log(`${habitablePlanets.length} Habitable planet(s) found!`);
-	for (const planet of habitablePlanets) {
-		console.log(planet.kepoi_name);
-	}
+  console.log(`${habitablePlanets.length} Habitable planet(s) found!`);
+  for (const planet of habitablePlanets) {
+    console.log(planet.kepoi_name);
+  }
 });
 
 createReadStream(csvFilePath)
-	.pipe(parser)
-	.on("error", function (error) {
-		console.log(error);
-	});
+  .pipe(parser)
+  .on("error", function (error) {
+    console.log(error);
+  });
 
 function isHabitablePlanet(planet: Planet): boolean {
-	return (
-		planet["koi_disposition"] === "CONFIRMED" &&
-		planet["koi_insol"] > 0.36 &&
-		planet["koi_insol"] < 1.11 &&
-		planet["koi_prad"] < 1.6
-	);
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
 }
 
 // ZTM solution
